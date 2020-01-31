3d84615091a5072667669270bd86880b997c91da
Shrey Sudan <shrsudan@publicisgroupe.net>
2020-01-31 11:23:52 +0530

    Updated Heading

--- packages/shared/dist-ui/atoms/Heading/Heading.jsx
+++ packages/shared/dist-ui/atoms/Heading/Heading.jsx
@@ -2,8 +2,7 @@ import React from "react";
 
 const Heading = props => {
   const HeadingLevel = `h${props.level}`;
-  return;
-  <HeadingLevel>{props.content}</HeadingLevel>;
+  return <HeadingLevel>{props.content}</HeadingLevel>;
 };
 
 export default Heading;
