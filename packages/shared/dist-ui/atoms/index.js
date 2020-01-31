6d444a3e70a0fe6102429a0885d91d1f3132a66a
Shrey Sudan <shrsudan@publicisgroupe.net>
2020-01-31 14:21:33 +0530

    Updated index.js of shared/dist-ui

--- packages/shared/dist-ui/atoms/index.js
+++ packages/shared/dist-ui/atoms/index.js
@@ -2,4 +2,3 @@ export { default as AnchorTag } from "./AnchorTag/AnchorTag";
 export { default as ErrorMessage } from "./ErrorMessage/ErrorMessage";
 export { default as Heading } from "./Heading/Heading";
 export { default as Img } from "./Img/Img";
-export { default as LazyLoad } from "./LazyLoad/LazyLoad";
--- packages/shared/dist-ui/index.js
+++ packages/shared/dist-ui/index.js
@@ -2,4 +2,3 @@ export { default as AnchorTag } from "./atoms/AnchorTag/AnchorTag";
 export { default as ErrorMessage } from "./atoms/ErrorMessage/ErrorMessage";
 export { default as Heading } from "./atoms/Heading/Heading";
 export { default as Img } from "./atoms/Img/Img";
-export { default as LazyLoad } from "./atoms/LazyLoad/LazyLoad";
